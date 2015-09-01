d0cd4cb47c53f2355c1644db2562f50d6def7bbf
Gabriel Florit <gabrielflorit@gmail.com>
2015-09-01 16:12:43 -0400

    make demo work

---
+++ demo.js
@@ -0,0 +1,15 @@
+var typewriter = require('./index.js');
+
+typewriter.prepare('.typewriter');
+
+// typewriter.type('.typewriter');
+
+// OR you could do the following, for greater control:
+typewriter.type('.three')
+.then(function() {
+	return typewriter.type('.two');
+})
+.then(function() {
+	typewriter.type('.one');
+});
+
